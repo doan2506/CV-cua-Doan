b1b715fcb48b36f266333d3b3c4f6ecc6a8ee69f
doan69420 <127383952+doan69420@users.noreply.github.com>
2023-03-17 20:19:42 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,40 @@
+function myFunction1() {
+    var x = document.getElementById("dropdown-content1");
+    if(x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction2() {
+    var x = document.getElementById("dropdown-content2");
+    if(x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction3() {
+    var x = document.getElementById("dropdown-content3");
+    if(x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction4() {
+    var x = document.getElementById("dropdown-content4");
+    if(x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function myFunction5() {
+    var x = document.getElementById("dropdown-content5");
+    if(x.style.display == "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
